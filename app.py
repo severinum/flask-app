d7ca7e1d1aaf9f5ddcccb7c2b938bf1d7eedf5bb
severinum <sewerynmichota@gmail.com>
2020-08-20 13:15:25 +0000

    /product GET - get list of all products

--- app.py
+++ app.py
@@ -61,7 +61,12 @@ def add_product():
 
     return product_schema.jsonify(new_product)
 
-
+# get all products
+@app.route('/product', methods=['GET'])
+def get_products():
+    all_products = Product.query.all()
+    result = products_schema.dump(all_products)
+    return jsonify(result)
 
 
 # Run server
